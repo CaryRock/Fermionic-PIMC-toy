557d99a48297720cb2937d6dcde3ca1eba8ee417
Thaumaturgen <carytherock@gmail.com>
2022-01-05 12:14:48 -0500

    Just cleaining this folder up some.

--- JULIA/format_output.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-name=$1
-shift
-for file in "$@"
-do
-    pimcave.py $file | grep $(head -n 1 "$file") | awk '{print $2}' >> "$name".dat
-    pimcave.py $file | grep $(head -n 1 "$file") | awk '{print $3}' >> "$name"_err.dat
-done
--- JULIA/temperature_analysis_format.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-name=$1
-file=$2
-
-pimcave.py $file | awk '{ print $1,'\t',$2,'\t',$3 }' > "$name".dat
