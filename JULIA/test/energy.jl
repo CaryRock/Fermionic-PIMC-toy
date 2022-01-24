d556b825013f2627e01f7f07e1baaa88a6900332
Thaumaturgen <carytherock@gmail.com>
2022-01-23 19:52:23 -0500

    Altered/added alternate ComputeAction() to speed up CenterOfMassMove(); overall speedup of ~35%

---
+++ JULIA/test/energy.jl
@@ -0,0 +1,48 @@
+# This file contains most of the code relevant to the energy aspect of the 
+# program, sans "ExtPotential" which remains in the main file (for now).
+# Energy estimators are to go here.
+
+# This is the basic method of computing the KE estimator. This shouldn't be 
+# used. Instead, the thermodynamic KE esetimator will be used.
+function KineticEnergy(Param, beads::Matrix{Float64})
+    # Computes the KE of the particle(s)
+    #dim = 1.0   # Float to aid julia's casting
+    tot = 0.0
+    norm = 1.0/(4.0 * Param.lam * Param.tau * Param.tau)
+    for tSlice = 1:Param.nTsl
+        tModPlus = ModTslice(tSlice + 1, Param.nTsl)
+        for ptcl = 1:Param.nPar
+            delR = beads[tModPlus,ptcl] - beads[tSlice,ptcl]
+            
+            tot = tot - norm * delR * delR
+        end
+    end
+    energy = 0.5 * Param.nPar / Param.tau + tot/Param.nTsl
+    return energy
+end
+
+function PotentialEnergy(Param::Params, potentials::Matrix{Float64})
+    # Computes the potential energy of the particle(s)
+    pe = 0.0
+
+    for tSlice = 1:Param.nTsl
+        for ptcl = 1:Param.nPar
+            tModPlus = ModTslice(tSlice + 1, Param.nTsl)
+#            R = beads[tSlice,ptcl]
+#            Rplus = beads[tModPlus,ptcl]
+#            pe += ExtPotential(lam,R) + ExtPotential(lam,Rplus)
+            pe += potentials[tSlice, ptcl] + potentials[tModPlus,ptcl]
+        end
+    end
+    return pe / (2*Param.nTsl)
+end
+
+function Energy(Param::Params, Path::Paths)
+    # Returns the KE + PE of the particle(s)
+
+    Path.KE = KineticEnergy(Param, Path.beads)
+    Path.PE = PotentialEnergy(Param, Path.potentials)
+    
+    return (Path.KE + Path.PE)
+end
+
