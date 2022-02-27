97fdc1289b3107aa089bd783695b003fe2292405
Thaumaturgen <carytherock@gmail.com>
2022-02-27 17:17:59 -0500

    Update

--- JULIA/test/N-particle_PF_at_T.py
+++ JULIA/test/N-particle_PF_at_T.py
@@ -1,6 +1,13 @@
 import numpy as np
 import sys
 
+def class PF:
+    # Partition function class. Each should have at least a Z and an eZ
+    # for recursion
+    __init__(L):
+        self.Z = np.zeros(L)
+        self.eZ = np.zeros(L)
+
 def Single_Energy(j):
     return j + 0.5
 
@@ -14,7 +21,8 @@ def Partition(energy, T, L):
 
 def main():
     T = float(sys.argv[1])
-    
+    N = int(sys.argv[2])
+
     hbar = 1.0
     w = 1.0
     kB = 1.0
@@ -23,22 +31,25 @@ def main():
     energy = np.zeros(L)
     for i in range(L):
         energy[i] = Single_Energy(i)
-
-    Z1 = np.zeros(L)
-    Z2 = np.zeros(L)
-    eZ1 = np.zeros(L)
-    eZ2 = np.zeros(L)
+    
+    Zarray = [PF(L) for i in range(N)]
+    
+    Z1 = np.zeros(L)    # Zarray[0].Z
+    Z2 = np.zeros(L)    # Zarray[1].Z
+    eZ1 = np.zeros(L)   # Zarray[0].eZ
+    eZ2 = np.zeros(L)   # Zarray[1].eZ
     for i in range(L):
         for j in range(L):
-            #Z1[i] = Partition(energy, T, L)
+            #Zarray[i].Z += np.exp(-(energy[i] + energy[j])/T)
             Z1[i] += np.exp(-(energy[i] + energy[j])/T)
+            #Zarray[i].eZ+= (energy[i] + energy[j]) * np.exp(-(energy[i] + energy[j])/T)
             eZ1[i] += (energy[i] + energy[j]) * np.exp(-(energy[i] + energy[j])/T)
         #Z2[i] = Partition(2.0*energy, T, L)
         Z2[i] = np.exp(-2.0 * energy[i] / T)
         eZ2[i] = 2.0 * energy[i] * np.exp(-2.0 * energy[i] / T)
     
     denom = sum(Z1) + sum(Z2)
-    
+    # denon = np.sum(Zarray)
     numer = 0.0
     for i in range(L):
         for j in range(L):
