7241c843ed381372d62158258f4db63460cb5e33
Thaumaturgen <carytherock@gmail.com>
2022-02-28 08:28:16 -0500

    Almost completed recursive partition and energy finder for fermions

--- JULIA/test/N-fermion_PF_at_T.py
+++ JULIA/test/N-fermion_PF_at_T.py
@@ -7,7 +7,7 @@ def Single_Energy(j):
 # Partition() through Deriv_SofK() can be condensed
 def Partition(energy, T, L):
     sum = 0.0
-
+    
     for i in range(L):
         sum += np.exp(-energy[i] / T)
     
@@ -30,35 +30,53 @@ def Deriv_Partition(energy, T, L):
     return -sum
 
 def Deriv_SofK(energy, T, L, mult):
-   sum = 0.0
+    sum = 0.0
 
-   for i in rage(L):
-       sum += energy[i] * np.exp(-energy[i] * mult / T)
+    for i in range(L):
+        sum += energy[i] * np.exp(-energy[i] * mult / T)
 
-    return -mult * sum
+    return (-mult * sum)
 
 def main():
     T = float(sys.argv[1])
     N = int(sys.argv[2])
+    Nplus = N + 1
 
     hbar = 1.0
     w = 1.0
     kB = 1.0
     L = 10  # Cutoff - the number of energy levels to compute to approximate "infinity"
-
+    eta = -1
     energy = np.zeros(L)
     for i in range(L):
         energy[i] = Single_Energy(i)
     
-    Zarray = [0.0 for i in range(N + 1)] # Array to store PFs - N+1 because 0 -> N
+    Z_array = [1.0 for i in range(Nplus)] # Array to store PFs - N+1 because 0 -> N
+    dZdB_array = [1.0 for i in range(Nplus)]
+    Sk_array = [1.0 for i in range(Nplus)]
+    dSkdB_array = [1.0 for i in range(Nplus)]
 
+    for i in range(Nplus):
+        Z_array[i] = Partition((N - i) * energy, T, L)   # Produces Zarray[Z_N, Z_N-1, ..., Z_1, Z_0=1.0]
+        Sk_array[i] = SofK(energy, T, L, i)
+        dZdB_array[i] = Deriv_Partition((N - i) * energy, T, L)
+        dSkdB_array[i] = Deriv_SofK(energy, T, L, i)
 
+    Z_array[-1] = 1.0   # Naive implementation leaves these as "= L" otherwise
+    Sk_array[0] = 1.0
+    print(Z_array)
+    print(Sk_array)
+    print(dZdB_array)
+    print(dSkdB_array)
 
+    #denom = np.sum(Z_array)    # If these were bosons, this would work
+    denom = 0.0
+    numer = 0.0
+    for i in range(Nplus):
+        denom += eta**(i) * Sk_array[i] * Z_array[N - i]
+        numer += eta**(i) * (dSkdB_array[i]*Z_array[N - i] + Sk_array[i]*dZdB_array[N - i])
 
-
-
-
-
+    print(f"Energy using recursion: {numer / denom}")
 
     
     Z1 = np.zeros(L)    # Zarray[0].Z
