aebeb2341629e58a8325a3b6cbdc05d1fecaacf8
Thaumaturgen <carytherock@gmail.com>
2022-03-01 15:24:24 -0500

    Prepping to send to workstation

--- JULIA/test/N-fermion_PF_at_T.py
+++ JULIA/test/N-fermion_PF_at_T.py
@@ -13,45 +13,61 @@ def Single_Energy(j):
     return j + 0.5
 
 class PF:
-    def __init__(self, E_function, T):
-        self.E = E_function
-        self.T = T
+    def __init__(self, T, L, energy):
+        self.T = T                  # The temperature
+        self.L = L                  # The cutoff length
+        self.Z = 0.0                # The value of the partition function
+        self.dZdB = 0.0             # The value of the derivative of Z wrt beta
+        self.energy = energy        # The energy spectrum
 
-    def get_cutoff_length(T):
-        
-
-# Partition() through Deriv_SofK() can be condensed
-def Partition(energy, T, L):
-    sum = 0.0
+    def Partition(self, zero=0):
+        if (zero != 0):
+            sum = 0.0
     
-    for i in range(L):
-        sum += np.exp(-energy[i] / T)
+            for i in range(self.L):
+                sum += np.exp(-self.energy[i] / self.T)
     
-    return sum
+            self.Z = sum
 
-def SofK(energy, T, L, mult):
-    sum = 0.0
+        else:
+            self.Z = 1.0
 
-    for i in range(L):
-        sum += np.exp(-energy[i] * mult / T)
+    def Deriv_Partition(self, zero=0):
+        if (zero != 0):
+            sum = 0.0
 
-    return sum
+            for i in range(self.L):
+                sum += self.energy[i] * np.exp(-self.energy[i] / self.T)
 
-def Deriv_Partition(energy, T, L):
-    sum = 0.0
+                self.dZdB = sum
 
-    for i in range(L):
-        sum += energy[i] * np.exp(-energy[i] / T)
+        else:
+            self.dZdB = np.sum(self.energy)
 
-    return -sum
-
-def Deriv_SofK(energy, T, L, mult):
-    sum = 0.0
-
-    for i in range(L):
-        sum += energy[i] * np.exp(-energy[i] * mult / T)
-
-    return (-mult * sum)
+class Sk:
+    def __init__(self, T, L, energy, multiple):    # "multiple" should never = 0
+        self.T = T                  # The temperature
+        self.L = L                  # The cutoff length
+        self.k = multiple           # The "k" of S(k)
+        self.Sk = 0.0               # The value of the S(k) function
+        self.dSkdB = 0.0            # The derivative of S(k) wrt beta
+        self.energy = energy        # The energy spectrum previously computed
+    
+    def SofK(self):
+        sum = 0.0
+    
+        for i in range(self.L):
+            sum += np.exp(-self.energy[i] * self.k / self.T)
+    
+        return sum
+    
+    def Deriv_SofK(self):
+        sum = 0.0
+    
+        for i in range(self.L):
+            sum += self.energy[i] * np.exp(-self.energy[i] * self.k / self.T)
+    
+        return (-self.k * sum)
 
 def main():
     T = float(sys.argv[1])
@@ -61,8 +77,7 @@ def main():
     hbar = 1.0
     w = 1.0
     kB = 1.0
-    L = 10  # Cutoff - the number of energy levels to compute to approximate "infinity"
-    # TODO: IMPLEMENT TEMPERATURE CUTOFF FOR COMPUTING ENERGY LEVELS
+    L = int(10.0 * T)  # Cutoff - the number of energy levels to compute
     # When is there a sufficient number of energy levels taken? When E/T >> 1 => e^-(E/T) approx 1
     eta = -1
     energy = np.zeros(L)
@@ -75,10 +90,10 @@ def main():
     and have each Z record its own energy spectrum, Sk_array, and the corresponding
     derivatives.
     """
-
-    Z_array = [1.0 for i in range(Nplus)] # Array to store PFs - N+1 because 0 -> N
+    '''
+    Z_array = [PF(T, L, energy) for i in range(Nplus)] # Array to store PFs - N+1 because 0 -> N
     dZdB_array = [1.0 for i in range(Nplus)]
-    Sk_array = [1.0 for i in range(Nplus)]
+    Sk_array = [Sk(T, L, energy, i) for i in range(1, Nplus)]
     dSkdB_array = [1.0 for i in range(Nplus)]
 
     for i in range(Nplus):
@@ -87,22 +102,22 @@ def main():
         dZdB_array[i] = Deriv_Partition((N - i) * energy, T, L)
         dSkdB_array[i] = Deriv_SofK(energy, T, L, i)
 
-    Z_array[-1] = 1.0   # Naive implementation leaves these as "= L" otherwise
-    Sk_array[0] = 1.0
-    print(Z_array)
-    print(Sk_array)
-    print(dZdB_array)
-    print(dSkdB_array)
+    Z_array[-1].Partition(0)
+    #Sk_array[0].SofK(0) = 1.0  # There shouldn't be a "S(0)" term
+    #print(Z_array)
+    #print(Sk_array)
+    #print(dZdB_array)
+    #print(dSkdB_array)
 
     #denom = np.sum(Z_array)    # If these were bosons, this would work
     denom = 0.0
     numer = 0.0
-    for i in range(Nplus):
+    for i in range(1,Nplus):
         denom += eta**(i) * Sk_array[i] * Z_array[N - i]
         numer += eta**(i) * (dSkdB_array[i]*Z_array[N - i] + Sk_array[i]*dZdB_array[N - i])
 
     print(f"Energy using recursion: {numer / denom}")
-
+    '''
     
     Z1 = np.zeros(L)    # Zarray[0].Z
     Z2 = np.zeros(L)    # Zarray[1].Z
@@ -110,16 +125,12 @@ def main():
     eZ2 = np.zeros(L)   # Zarray[1].eZ
     for i in range(L):
         for j in range(L):
-            #Zarray[i].Z += np.exp(-(energy[i] + energy[j])/T)
             Z1[i] += np.exp(-(energy[i] + energy[j])/T)
-            #Zarray[i].eZ+= (energy[i] + energy[j]) * np.exp(-(energy[i] + energy[j])/T)
             eZ1[i] += (energy[i] + energy[j]) * np.exp(-(energy[i] + energy[j])/T)
-        #Z2[i] = Partition(2.0*energy, T, L)
         Z2[i] = np.exp(-2.0 * energy[i] / T)
         eZ2[i] = 2.0 * energy[i] * np.exp(-2.0 * energy[i] / T)
     
     denom = sum(Z1) - sum(Z2)
-    # denon = np.sum(Zarray)
     numer = 0.0
     for i in range(L):
         for j in range(L):
@@ -130,7 +141,7 @@ def main():
 
     E = numer / denom
 
-    print(f"E is approximately: {E}")
+    print(f"T = {T}\tE = {E}")
 
 if __name__ == "__main__":
     main()
