5de305b0bbc143748687eebbfd4249f50f626250
CaryRock <carytherock@gmail.com>
2022-03-02 08:24:15 -0500

    Sinking

--- JULIA/test/fermion_boson_comparison.py
+++ JULIA/test/fermion_boson_comparison.py
@@ -1,10 +1,32 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse as ap
+import pathlib as pl
 
-def main():
-    bosons = sys.argv[1]
-    fermions = sys.argv[2]
+def create_parser():
+    parser = ap.ArgumentParser(description='Recursive partition function \
+            solver. Computes desired quantities from the PF. Designed with \
+            using GNU paralell in mind and storing output "manually".',
+            formatter_class=ap.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-b", "--bosons", type=pl.Path, help="File containing \
+            boson data.")
+    parser.add_argument("-f", "--fermions", type=pl.Path, help="File \
+            containing fermion data.")
+    parser.add_argument("-log", "--logy", action='store_true', help="Use \
+            numpy's semilogy to plot the results.")
+
+    return parser
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    parser = create_parser()
+    args = parser.parse_args(argv[1:])
+
+    bosons = args.bosons
+    fermions = args.fermions
 
     with open(bosons, "r") as bos:
         boson_data = np.loadtxt(bos)
@@ -19,8 +41,14 @@ def main():
         print(fermions[i] - bosons[i])
     
     temps = boson_data[:,0]#np.linspace(1, len(boson_data), len(boson_data))
-    plt.plot(temps, bosons, 'b.', label="Boson <E>, N = 2")
-    plt.plot(temps, fermions, 'r.', label="Fermion <E>, N = 2")
+    if not args.logy:
+        plt.plot(temps, bosons, 'b.', label="Boson <E>, N = 2")
+        plt.plot(temps, fermions, 'r.', label="Fermion <E>, N = 2")
+
+    else:
+        plt.semilogy(temps, bosons, 'b.')
+        plt.semilogy(temps, fermions, 'r.')
+
     plt.plot(temps, fermions - bosons, 'k.', label="Difference")
     plt.xlabel("T (K)")
     plt.ylabel("Energy (K)")
